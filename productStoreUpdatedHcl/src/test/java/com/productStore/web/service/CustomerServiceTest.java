f3d3d5a37a81cb67b9dc3b0caa85f0001eb46a84
RAJASEKHARTMK <RAJASEKHARTMK@DESKTOP-NFIF79L>
2019-12-05 00:32:08 +0530

    commit4

---
+++ productStoreUpdatedHcl/src/test/java/com/productStore/web/service/CustomerServiceTest.java
@@ -0,0 +1,78 @@
+package com.productStore.web.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.productStore.model.entities.Customer;
+import com.productStore.model.exceptions.CustomerNotFoundException;
+import com.productStore.model.persistance.CustomerRepositery;
+import com.productStore.model.service.CustomerServiceImpl;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class CustomerServiceTest {
+
+	@InjectMocks
+	CustomerServiceImpl custService;
+	
+	@Mock
+	CustomerRepositery custRepository;
+	
+	static Customer customer=new Customer();
+	static List<Customer> customers=new ArrayList<Customer>();
+	
+	@BeforeClass
+	public static void setup(){
+		customer.setId(1L);
+		customer.setEmail("email");
+		customer.setName("raj");
+		customers.add(customer);
+	}
+	
+	@Test
+	public void testFindByEmailPositive(){
+		Mockito.when(custRepository.findByEmail(Mockito.any())).thenReturn(customer);
+		Customer cust=custService.findByEmail("email");
+		Assert.assertNotNull(cust);
+		Assert.assertEquals("raj", cust.getName());
+	}
+	
+	@Test
+	public void testFindByEmailNegative(){
+		Mockito.when(custRepository.findByEmail("email")).thenReturn(customer);
+		Customer cust=custService.findByEmail("emai");
+		Assert.assertNull(cust);
+		//Assert.assertEquals("raj", cust.getName());
+	}
+	
+	@Test
+	public void testFindByIdPositive(){
+		Mockito.when(custRepository.findById(Mockito.any())).thenReturn(Optional.of(customer));
+		Customer cust=custService.findById(1L);
+		Assert.assertNotNull(cust);
+		Assert.assertEquals("raj", cust.getName());
+	}
+	
+	@Test(expected=CustomerNotFoundException.class)
+	public void testFindByIdNegativeException(){
+		Mockito.when(custRepository.findById(1L)).thenReturn(Optional.of(customer));
+		custService.findById(2L);
+	}
+	
+	@Test
+	public void testFindAll(){
+		Mockito.when(custRepository.findAll()).thenReturn(customers);
+		List<Customer> testcusts=custService.findAll();
+		Assert.assertNotNull(testcusts);
+		Assert.assertEquals(1, testcusts.size());
+	}
+}
---
+++ productStoreUpdatedHcl/src/test/java/com/productStore/web/service/OrderServiceTest.java
@@ -0,0 +1,96 @@
+package com.productStore.web.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.productStore.model.entities.Customer;
+import com.productStore.model.entities.Order;
+import com.productStore.model.persistance.CustomerRepositery;
+import com.productStore.model.persistance.OrderRepositery;
+import com.productStore.model.service.OrderServiceImpl;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class OrderServiceTest {
+	@InjectMocks
+	OrderServiceImpl ordService;
+	
+	@Mock
+	OrderRepositery ordRepository;
+	
+	@Mock
+	CustomerRepositery custRepo;
+	
+	static Customer customer=new Customer();
+	static Order order = new Order();
+	static List<Order> orders = new ArrayList<Order>();
+	
+	@BeforeClass
+	public static void setup(){
+		customer.setId(1L);
+		order.setId(1L);
+		order.setCustomerName("raj");
+		order.setProductName("phone");
+		order.setCustomer(customer);
+		orders.add(order);
+		customer.addOrder(order);
+	}
+	
+	@Test
+	public void testFindByCustomerNamePositive(){
+		Mockito.when(ordRepository.findByCustomerName(Mockito.any())).thenReturn(orders);
+		List<Order> testOrders=ordService.findByCustomerName("raj");
+		Assert.assertNotNull(testOrders);
+		Assert.assertEquals(1, testOrders.size());
+		
+	}
+	
+	@Test
+	public void testFindByProductNameContainingPositive(){
+		Mockito.when(ordRepository.findByProductNameContaining("ph")).thenReturn(orders);
+		List<Order> testOrders=ordService.findByProductNameContaining("ph");
+		Assert.assertNotNull(testOrders);
+		Assert.assertEquals(1, testOrders.size());
+	}
+	
+	@Test
+	public void testFindByCustomerNameNegative(){
+		Mockito.when(ordRepository.findByCustomerName("raj")).thenReturn(orders);
+		List<Order> testOrders=ordService.findByCustomerName("ra");
+		//Assert.assertNull(testOrders);
+		Assert.assertEquals(0, testOrders.size());
+		
+	}
+	
+	@Test
+	public void testFindByProductNameContainingNegative(){
+		Mockito.when(ordRepository.findByProductNameContaining("ph")).thenReturn(orders);
+		List<Order> testOrders=ordService.findByProductNameContaining("hp");
+		//Assert.assertNotNull(testOrders);
+		Assert.assertEquals(0, testOrders.size());
+	}
+	
+//	@Test
+//	public void testFindByCustomerId(){
+//		Mockito.when(ordRepository.findByCustomerId(1L)).thenReturn(orders);
+//		List<Order> testOrders=ordService.findByCustomerId(1L);
+//		Assert.assertNotNull(testOrders);
+//		Assert.assertEquals(1, testOrders.size());
+//	}
+	
+	@Test
+	public void testFindAll(){
+		Mockito.when(ordRepository.findAll()).thenReturn(orders);
+		List<Order> testOrders=ordService.findAll();
+		Assert.assertNotNull(testOrders);
+		Assert.assertEquals(1, testOrders.size());
+	}
+}
